735b80c329ed356b7db899abcbe03c3f6c9f71d9
AmirPerkins <148828267+AmirPerkins@users.noreply.github.com>
2023-12-04 23:25:41 -0500

    This is the final

--- projectJS.js
+++ projectJS.js
@@ -9,7 +9,7 @@ function checkEmail(){
 
     if (emailValidationPattern.test(userEmail)){
         console.log("success");
-        createHTML();
+        getUserData();
     }
     else{
         alert("Type in a proper email.");
@@ -17,49 +17,118 @@ function checkEmail(){
 }
 
 function clearInputs(){
-    breakfastMeal = document.getElementById("breakfast");
-    firstSnack = document.getElementById("firstSnack");
-    lunchMeal = document.getElementById("lunch");
-    secondSnack = document.getElementById("secondSnack");
-    dinner = document.getElementById("dinner");
-
-    breakfastMeal.value = "";
-    firstSnack.value = "";
-    lunchMeal.value = "";
-    secondSnack.value = "";
-    dinner.value = "";
+    for(i = 1; i < 8; i++){
+        currentDay = String(i)
+        document.getElementById("breakfast"+ currentDay).value = "";
+        document.getElementById("firstSnack"+ currentDay).value = "";
+        document.getElementById("lunch"+ currentDay).value = "";
+        document.getElementById("secondSnack"+ currentDay).value = "";
+        document.getElementById("dinner"+ currentDay).value = "";
+    }
 }
 
-function createHTML(){
-
+function getUserData(){
     userName = document.getElementById("name").value;
     userEmail = document.getElementById("email").value;
     userGoals = document.getElementById("goals").value;
+    userData = [userName, userEmail, userGoals]
 
-    breakfastMeal = document.getElementById("breakfast").value;
-    firstSnack = document.getElementById("firstSnack").value;
-    lunchMeal = document.getElementById("lunch").value;
-    secondSnack = document.getElementById("secondSnack").value;
-    dinner = document.getElementById("dinner").value;
-    meals = [breakfastMeal, firstSnack, lunchMeal, secondSnack, dinner];
-
-    newWindow = window.open('about:blank','myPop','width=1000,height=600,left=200,top=200');
-    newWindow.document.write("<h1>Alright, " + userName + ". Here is your meal plan!</h1><br>");
-    newWindow.document.write('<table>' + 
-        '<tr>' +
-          '<th>breakfast</th>' +
-          '<th>firstSnack</th>'+
-          '<th>Lunch</th>' +
-          '<th>Second Snack</th>' +
-          '<th>Dinner</th>' +
-        '</tr>');
-    newWindow.document.write('<tr>');
-    for(i = 0; i < 5; i++){
-    newWindow.document.write('<th>' + meals[i] + '</th>');
+    for(i = 1; i < 8; i++){
+        currentDay = String(i)
+        breakfastMeal = document.getElementById("breakfast"+ currentDay).value;
+        firstSnack = document.getElementById("firstSnack"+ currentDay).value;
+        lunchMeal = document.getElementById("lunch"+ currentDay).value;
+        secondSnack = document.getElementById("secondSnack"+ currentDay).value;
+        dinner = document.getElementById("dinner"+ currentDay).value;
+        switch(i) {
+            case 1:
+                mondayMeals = [breakfastMeal, firstSnack, lunchMeal, secondSnack, dinner];
+            case 2:
+                tuesdayMeals = [breakfastMeal, firstSnack, lunchMeal, secondSnack, dinner];
+            case 3:
+                wednesdayMeals = [breakfastMeal, firstSnack, lunchMeal, secondSnack, dinner];
+            case 4:
+                thursdayMeals = [breakfastMeal, firstSnack, lunchMeal, secondSnack, dinner];
+            case 5:
+                fridayMeals = [breakfastMeal, firstSnack, lunchMeal, secondSnack, dinner];
+            case 6:
+                saturadayMeals = [breakfastMeal, firstSnack, lunchMeal, secondSnack, dinner];
+            case 7:
+                sundayMeals = [breakfastMeal, firstSnack, lunchMeal, secondSnack, dinner];
+        }
     }
-    newWindow.document.write('</tr>');
+    meals = [mondayMeals, tuesdayMeals, wednesdayMeals, thursdayMeals, fridayMeals, saturadayMeals, sundayMeals];
+    createTable(userData, meals);
 
-    newWindow.document.write(breakfastMeal + firstSnack + lunchMeal + secondSnack + dinner);
 }
 
+function createTable(userData, meals){
+    let tableContent = [
+        ["Monday", meals[0]], 
+        ["Tuesday", meals[1]], 
+        ["Wednesday", meals[2]], 
+        ["Thursday", meals[3]], 
+        ["Friday", meals[4]], 
+        ["Saturday", meals[5]], 
+        ["Sunday", meals[6]]
+    ];
+    let day = 0;
+    let food = 1;
+    console.log(tableContent[0][1][0])
+    newWindow = window.open('about:blank','myPop','width=1000,height=600,left=200,top=200');
+    newWindow.document.write("<h1>Alright, " + userData[0] + ". Here is your meal plan!</h1><br>");
+    let table = document.createElement("table");
+    table.id = "mealTable";
+    let mealRowLabel = '<tr>' +
+      '<th></th>' +
+      '<th>breakfast</th>' +
+      '<th>firstSnack</th>'+
+      '<th>Lunch</th>' +
+      '<th>Second Snack</th>' +
+      '<th>Dinner</th>' +
+    '</tr>';
+    table.innerHTML = mealRowLabel;
+    mealPlan = newWindow.document.body.appendChild(table);
+    
+    for(weekday = 0; weekday < 7; weekday++){
+        dayRow = mealPlan.insertRow();
+        newDay = dayRow.insertCell();
+        newDay.innerHTML = tableContent[weekday][day];
+        console.log(dayRow);
+        for(meal = 0; meal < 5; meal++){
+            newMeal = dayRow.insertCell();
+            newMeal.innerHTML = tableContent[weekday][food][meal];
+        }
+    }
+
+    printButton = document.createElement("button");
+    printButton.id = "printable";
+    printButton.innerHTML = "Print mealplan";
+    printButton.addEventListener("click", function(){
+        newWindow.print();
+    });
+    newWindow.document.body.appendChild(printButton);
+
+    downloadButton = document.createElement("button");
+    downloadButton.id = "download";
+    downloadButton.innerHTML = "Download mealplan";
+    downloadButton.addEventListener("click", downloadPlanner(newWindow));
+    console.log(table);
+}
+
+function downloadPlanner(newWindow) {
+    let table = newWindow.document.getElementById("mealTable")
+    let planner = table.textContent; 
+    let filename = "mealplan.html";
+  
+    let link = document.createElement("a");
+    link.setAttribute("href", "data:text/html;charset=utf-8," + encodeURIComponent(planner));
+    link.setAttribute("download", filename);
+    link.style.display = "none"; 
+
+    document.body.appendChild(link);
+    link.click();
+  
+    document.body.removeChild(link);
+  }
 main();
